8c6ae338ec3165432c10d8da5c4857507af68a46
Lilian Chu <lilian.emchu@gmail.com>
2019-03-29 11:25:01 -0700

    Began SpawnChild Script
    -Created various layers and tags for organizaional purpose
    -Began SpawnChild script. Will refine within the day

---
+++ Bready for Action/Assets/Scripts/SpawnChild.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnChild : MonoBehaviour
+{
+
+    public GameObject[] childTypes;
+    public Transform[] spawnPositions;
+
+    [HideInInspector] public int typePos;
+
+    private static float spawnTimeCounter;
+    private float cooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        typePos = 0;
+        spawnTimeCounter = 0f;
+        cooldown = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (spawnTimeCounter < 0))
+        {
+            //Instantiate(childTypes[typePos], SpawnChild.ChoosePos(), new Quaternion(0,0,0,0));
+            Instantiate(childTypes[typePos], spawnPositions[0].position, new Quaternion(0, 0, 0, 0));
+            spawnTimeCounter = cooldown;
+        }
+        spawnTimeCounter -= Time.deltaTime;
+    }
+
+
+    /*
+    private Vector3 ChoosePos()
+    {
+        for (int i = 0; i < spawnPositions.Length; i++)
+        {
+            if (!hitToTest.collider.bounds.Contains(telePosition))
+            {
+                print("point is inside collider");
+            }
+        }
+    }
+    */
+}
